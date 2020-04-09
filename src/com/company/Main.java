1eb429afc433c58319d6554f9dd6ebc4e8d21532
Leonardo <leonardo.lelli3@studio.unibo.it>
2020-04-09 16:17:55 +0200

    Modifica molto importante

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Ciao bimbi !!");
+        for(int i=0 ; i<10; i++)
+            System.out.println("CHESANI E' BELLO");
     }
 }
