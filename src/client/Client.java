f7c0b257b7e9148259dc19f36aaf3fe3fa9fc800
tullo <tullini.lorenzo@gmail.com>
2020-04-09 08:38:52 -0700

    Modifico client ancora !!

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,5 +4,6 @@ public class Client {
     public static void main(String argv[]) {
         System.out.println("Ciao a tutti !!");
         System.out.println("Ciao a tutti versione 2!!");
+        System.out.println("Ciao a tutti versione 3!!");
     }
 }
