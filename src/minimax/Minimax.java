b2d5efe8b45c87d5f86fa7e0e70816f89059538a
Federico <marchetti.federico@protonmail.com>
2020-04-13 10:05:43 +0200

    Aggiunto controllo sulle partite patte

--- src/minimax/Minimax.java
+++ src/minimax/Minimax.java
@@ -4,59 +4,55 @@ import client.TimeManager;
 import model.Move;
 import model.PlayerType;
 import model.TableState;
+import org.jetbrains.annotations.NotNull;
 
-import java.time.Duration;
-import java.time.Instant;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinTask;
-import java.util.stream.Collectors;
 
 
 public class Minimax {
     private final int maxDepth;
-    //TODO Le euristiche devono tenere conto anche della possibilità che una mossa possa condurre alla vittoria
-    private static IHeuristic[] whiteEheuristic = null;
-    private static IHeuristic[] blackEheuristic = null;
     //L'avversario gioca sempre come min
     private final PlayerType myColour, opponentColour;
     private final IHeuristic[] myHeuristic, opponentHeuristic;
+    private final Set<Integer> alreadyVisitedStates;
+    private final ForkJoinPool fjPool;
 
-    public Minimax(PlayerType myColour, int maxDepth) {
-        if (whiteEheuristic == null || blackEheuristic == null) {
-            //se non sono già stati inizializzati
-            whiteEheuristic = new IHeuristic[3];
-            blackEheuristic = new IHeuristic[3];
+    public Minimax(PlayerType myColour, int maxDepth, int threadNumber) {
+        IHeuristic[] whiteEheuristic = new IHeuristic[3];
+        IHeuristic[] blackEheuristic = new IHeuristic[3];
 
-            //inizializzazione euristiche
-            whiteEheuristic[0] = (TableState s, int depth) ->
-                    s.getBlackPiecesCount() - s.getBlackPiecesCount() + s.getKingDistance() +
-                            ((s.hasWhiteWon()) ? 100 + maxDepth - depth : 0) +
-                            ((s.hasBlackWon()) ? -(100 + maxDepth - depth) : 0);
-            whiteEheuristic[1] = (TableState s, int depth) ->
-                    s.getBlackPiecesCount() - s.getBlackPiecesCount() + 2 * s.getKingDistance() +
-                            ((s.hasWhiteWon()) ? 100 + maxDepth - depth : 0) +
-                            ((s.hasBlackWon()) ? -(100 + maxDepth - depth) : 0);
-            whiteEheuristic[2] = (TableState s, int depth) ->
-                    s.getBlackPiecesCount() - s.getBlackPiecesCount() + 3 * s.getKingDistance() +
-                            ((s.hasWhiteWon()) ? 100 + maxDepth - depth : 0) +
-                            ((s.hasBlackWon()) ? -(100 + maxDepth - depth) : 0);
+        //inizializzazione euristiche
+        whiteEheuristic[0] = (TableState s, int depth) ->
+                s.getBlackPiecesCount() - s.getBlackPiecesCount() + s.getKingDistance() +
+                        ((s.hasWhiteWon()) ? 100 + maxDepth - depth : 0) +
+                        ((s.hasBlackWon()) ? -(100 + maxDepth - depth) : 0);
+        whiteEheuristic[1] = (TableState s, int depth) ->
+                s.getBlackPiecesCount() - s.getBlackPiecesCount() + 2 * s.getKingDistance() +
+                        ((s.hasWhiteWon()) ? 100 + maxDepth - depth : 0) +
+                        ((s.hasBlackWon()) ? -(100 + maxDepth - depth) : 0);
+        whiteEheuristic[2] = (TableState s, int depth) ->
+                s.getBlackPiecesCount() - s.getBlackPiecesCount() + 3 * s.getKingDistance() +
+                        ((s.hasWhiteWon()) ? 100 + maxDepth - depth : 0) +
+                        ((s.hasBlackWon()) ? -(100 + maxDepth - depth) : 0);
 
-            //TODO aggiugnere conteggio dei pezzi in ognuno dei quattro angoli
-            blackEheuristic[0] = (TableState s, int depth) ->
-                    s.getBlackPiecesCount() - s.getWhitePiecesCount() - s.getKingDistance() +
-                            ((s.hasBlackWon()) ? 100 + maxDepth - depth : 0) +
-                            ((s.hasWhiteWon()) ? -(100 + maxDepth - depth) : 0);
-            blackEheuristic[1] = (TableState s, int depth) ->
-                    2 * (s.getBlackPiecesCount() - s.getWhitePiecesCount()) - 1.5 * s.getKingDistance() +
-                            ((s.hasBlackWon()) ? 100 + maxDepth - depth : 0) +
-                            ((s.hasWhiteWon()) ? -(100 + maxDepth - depth) : 0);
-            blackEheuristic[2] = (TableState s, int depth) ->
-                    3 * (s.getBlackPiecesCount() - s.getWhitePiecesCount()) - 1.5 * s.getKingDistance() +
-                            ((s.hasBlackWon()) ? 100 + maxDepth - depth : 0) +
-                            ((s.hasWhiteWon()) ? -(100 + maxDepth - depth) : 0);
-        }
+        //TODO aggiugnere conteggio dei pezzi in ognuno dei quattro angoli
+        blackEheuristic[0] = (TableState s, int depth) ->
+                s.getBlackPiecesCount() - s.getWhitePiecesCount() - s.getKingDistance() +
+                        ((s.hasBlackWon()) ? 100 + maxDepth - depth : 0) +
+                        ((s.hasWhiteWon()) ? -(100 + maxDepth - depth) : 0);
+        blackEheuristic[1] = (TableState s, int depth) ->
+                2 * (s.getBlackPiecesCount() - s.getWhitePiecesCount()) - 1.5 * s.getKingDistance() +
+                        ((s.hasBlackWon()) ? 100 + maxDepth - depth : 0) +
+                        ((s.hasWhiteWon()) ? -(100 + maxDepth - depth) : 0);
+        blackEheuristic[2] = (TableState s, int depth) ->
+                3 * (s.getBlackPiecesCount() - s.getWhitePiecesCount()) - 1.5 * s.getKingDistance() +
+                        ((s.hasBlackWon()) ? 100 + maxDepth - depth : 0) +
+                        ((s.hasWhiteWon()) ? -(100 + maxDepth - depth) : 0);
+
 
         this.maxDepth = maxDepth;
 
@@ -64,52 +60,74 @@ public class Minimax {
         this.opponentColour = (myColour == PlayerType.WHITE) ? PlayerType.BLACK : PlayerType.WHITE;
 
         this.myHeuristic = (myColour == PlayerType.WHITE) ? whiteEheuristic : blackEheuristic;
-        this.opponentHeuristic = (myColour == PlayerType.BLACK) ? whiteEheuristic : blackEheuristic;
+        this.opponentHeuristic = (myColour == PlayerType.WHITE) ? blackEheuristic : whiteEheuristic;
+
+        this.alreadyVisitedStates = new HashSet<>();
+
+        this.fjPool = new ForkJoinPool(threadNumber);
     }
 
 
-    public Move minimax(TableState initialState, TimeManager gestore, int turn) {
-        return performMinimax(initialState, gestore, true, turn, 0, null);
+    public Move minimax(@NotNull TableState initialState, TimeManager gestore, int turn) {
+        //aggiungi lo stato prodotto dall'avversario
+        alreadyVisitedStates.add(initialState.hashCode());
+        Move res = performMinimax(initialState, gestore, true, turn, 0, null);
+        //aggiungi lo stato appena prodotto
+        alreadyVisitedStates.add(initialState.performMove(res).hashCode());
+        return res;
     }
 
-    public Move alphabeta(TableState initialState, TimeManager gestore, int turn) {
-        return performAlphabeta(initialState, gestore, true, turn, 0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, null);
+    public Move alphabeta(@NotNull TableState initialState, TimeManager gestore, int turn) {
+        //aggiungi lo stato prodotto dall'avversario
+        alreadyVisitedStates.add(initialState.hashCode());
+        Move res = performAlphabeta(initialState, gestore, true, turn, 0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, null);
+        //aggiungi lo stato appena prodotto
+        alreadyVisitedStates.add(initialState.performMove(res).hashCode());
+        return res;
     }
 
-    public Move parallelAlphaBeta(TableState initialState, TimeManager gestore, int turn, int threadNumber) {
+    public Move parallelAlphaBeta(@NotNull TableState initialState, TimeManager gestore, int turn) {
         //Lancia in parallelo threadNumber thread paralleli che effettuano il calcolo su threadNumber sottoalberi distinti in parallelo.
         //Da un singolo problema di ricerca ne abbiamo ora n distinti che però possono essere eseguiti in parallelo
-        ForkJoinPool fjPool = new ForkJoinPool(threadNumber);
-        List<Move> allPossibleMoves = initialState.getAllMovesFor(myColour);
-        Optional<Move> result;
 
-        ForkJoinTask<List<Move>> task = fjPool.submit(() ->
+        List<Move> allPossibleMoves = initialState.getAllMovesFor(myColour);
+        Move result = null;
+
+        alreadyVisitedStates.add(initialState.hashCode());
+        ForkJoinTask<Move> task = fjPool.submit(() ->
                 allPossibleMoves.parallelStream().map((Move m) ->
                         performAlphabeta(initialState.performMove(m), gestore, false, turn + 1, 1, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, m)
-                ).collect(Collectors.toList()));
+                ).max((Move m1, Move m2) -> {
+                    int res = 0;
+
+                    if (m1.getCosto() < m2.getCosto()) {
+                        res = -1;
+                    } else if (m1.getCosto() > m2.getCosto()) {
+                        res = 1;
+                    }
+
+                    return res;
+                }).orElse(null));
 
         try {
-            result = task.get().stream().max((Move m1, Move m2) -> {
-                int res = 0;
-
-                if (m1.getCosto() < m2.getCosto()) {
-                    res = -1;
-                } else if (m1.getCosto() > m2.getCosto()) {
-                    res = 1;
-                }
-
-                return res;
-            });
+            result = task.get();
+            alreadyVisitedStates.add(initialState.performMove(result).hashCode());
         } catch (Exception e) {
-            result = Optional.empty();
+            result = null;
+            e.printStackTrace();
         }
 
-        return result.orElse(null);
+        return result;
     }
 
-    private Move performMinimax(TableState state, TimeManager gestore, boolean isMaxTurn, int turn, int currentDepth, Move performedMove) {
+    private Move performMinimax(@NotNull TableState state, TimeManager gestore, boolean isMaxTurn, int turn, int currentDepth, Move performedMove) {
+        if (alreadyVisitedStates.contains(state.hashCode())) {
+            performedMove.setCosto(0);
+            return performedMove;
+        }
+
         List<Move> allPossibleMoves = state.getAllMovesFor((isMaxTurn) ? myColour : opponentColour);
-        //TODO deve entare in questo if anche se si tratta di uno stato di vittoria
+
         if (currentDepth == maxDepth || gestore.isEnd() || allPossibleMoves.isEmpty() || state.hasBlackWon() || state.hasWhiteWon()) {
             //valuta il nodo corrente
             int heuristicIndex;
@@ -163,9 +181,14 @@ public class Minimax {
         return bestMove;
     }
 
-    private Move performAlphabeta(TableState state, TimeManager gestore, boolean isMaxTurn, int turn, int currentDepth, double alpha, double beta, Move performedMove) {
+    private Move performAlphabeta(@NotNull TableState state, TimeManager gestore, boolean isMaxTurn, int turn, int currentDepth, double alpha, double beta, Move performedMove) {
+        if (alreadyVisitedStates.contains(state.hashCode())) {
+            performedMove.setCosto(0);
+            return performedMove;
+        }
+
         List<Move> allPossibleMoves = state.getAllMovesFor((isMaxTurn) ? myColour : opponentColour);
-        //TODO deve entare in questo if anche se si tratta di uno stato di vittoria
+
         if (currentDepth == maxDepth || gestore.isEnd() || allPossibleMoves.isEmpty() || state.hasBlackWon() || state.hasWhiteWon()) {
             //valuta il nodo corrente
             int heuristicIndex;
@@ -213,6 +236,81 @@ public class Minimax {
             for (Move m : allPossibleMoves) {
                 newState = state.performMove(m);
                 performAlphabeta(newState, gestore, true, turn + 1, currentDepth + 1, alpha, beta, m);
+
+                if (m.getCosto() < bestCost) {
+                    bestCost = m.getCosto();
+                    bestMove = m;
+                }
+
+                beta = Math.min(beta, m.getCosto());
+                if (alpha >= beta) {
+                    return bestMove;
+                }
+            }
+        }
+        return bestMove;
+    }
+
+    //Controlla in maniera più accurata la presenza di stati già visitati
+    private Move performAlphabetaWithHistoryCheck(@NotNull TableState state, TimeManager gestore, boolean isMaxTurn, int turn, int currentDepth, double alpha, double beta, Move performedMove, @NotNull Set<Integer> previousHistory) {
+        //se questo stato è già stato raggiunto
+        if (previousHistory.contains(state.hashCode())) {
+            performedMove.setCosto(0);
+            return performedMove;
+        }
+
+        List<Move> allPossibleMoves = state.getAllMovesFor((isMaxTurn) ? myColour : opponentColour);
+
+        //se è ora di terminare l'esplorazione
+        if (currentDepth == maxDepth || gestore.isEnd() || allPossibleMoves.isEmpty() || state.hasBlackWon() || state.hasWhiteWon()) {
+            //valuta il nodo corrente
+            int heuristicIndex;
+            if (turn < 40) {
+                heuristicIndex = 0;
+            } else if (turn < 70) {
+                heuristicIndex = 1;
+            } else {
+                heuristicIndex = 2;
+            }
+            //Per scrupolo, probabilmente si può togliere
+            if (performedMove != null) {
+                performedMove.setCosto(isMaxTurn ?
+                        myHeuristic[heuristicIndex].evaluate(state, currentDepth) :
+                        -opponentHeuristic[heuristicIndex].evaluate(state, currentDepth));
+            }
+
+            return performedMove;
+        }
+
+        //esplora il sottoalbero
+        Move bestMove = null;
+        double bestCost;
+        //Ad ogni nodo si crea un universo alternativo che non deve andare ad avere effetti sugli altri universi
+        Set<Integer> myHistory = new HashSet<>(previousHistory);
+        myHistory.add(state.hashCode());
+        if (isMaxTurn) {
+            bestCost = Double.NEGATIVE_INFINITY;
+            TableState newState;
+            for (Move m : allPossibleMoves) {
+                newState = state.performMove(m);
+                performAlphabetaWithHistoryCheck(newState, gestore, false, turn + 1, currentDepth + 1, alpha, beta, m, myHistory);
+
+                if (m.getCosto() > bestCost) {
+                    bestCost = m.getCosto();
+                    bestMove = m;
+                }
+
+                alpha = Math.max(alpha, m.getCosto());
+                if (alpha >= beta) {
+                    return bestMove;
+                }
+            }
+        } else {
+            bestCost = Double.POSITIVE_INFINITY;
+            TableState newState;
+            for (Move m : allPossibleMoves) {
+                newState = state.performMove(m);
+                performAlphabetaWithHistoryCheck(newState, gestore, true, turn + 1, currentDepth + 1, alpha, beta, m, myHistory);
 
                 if (m.getCosto() < bestCost) {
                     bestCost = m.getCosto();
